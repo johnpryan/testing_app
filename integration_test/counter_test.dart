c491da57a53108157ce8c887c56195aa0f1e8c4e
John Ryan <ryjohn@google.com>
2020-11-25 13:57:56 -0800

    add two tests, skip screenshot test

--- integration_test/counter_test.dart
+++ integration_test/counter_test.dart
@@ -1,10 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:testing_app/main.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
+      as IntegrationTestWidgetsFlutterBinding;
 
-  testWidgets("failing test example", (WidgetTester tester) async {
+  testWidgets("passing test example", (WidgetTester tester) async {
     expect(2 + 2, equals(4));
+  });
+
+
+  testWidgets("take a screenshot", (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    await binding.takeScreenshot('Tapped Plus Button');
+  }, skip: true /* Not working on iOS, Android or Chrome */);
+
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
