ad23a10c6ad69875807fe6276237d22c6ca0c8ba
John Ryan <ryjohn@google.com>
2020-11-25 14:27:14 -0800

    use recommended pattern for running the app,

--- integration_test/counter_test.dart
+++ integration_test/counter_test.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:testing_app/main.dart';
+import 'package:testing_app/main.dart' as app;
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
       as IntegrationTestWidgetsFlutterBinding;
 
   testWidgets("passing test example", (WidgetTester tester) async {
+    app.main();
+
     expect(2 + 2, equals(4));
   });
 
-
   testWidgets("take a screenshot", (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    app.main();
+
+    await tester.pumpAndSettle();
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
@@ -24,8 +26,9 @@ void main() {
   }, skip: true /* Not working on iOS, Android or Chrome */);
 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    app.main();
+
+    await tester.pumpAndSettle();
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -38,5 +41,22 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Increment counter performance test',
+      (WidgetTester tester) async {
+    app.main();
+
+    await binding.traceAction(() async {
+      await tester.pumpAndSettle();
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+
+    });
   });
 }
